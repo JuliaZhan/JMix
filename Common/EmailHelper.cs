1787bdeb87ee8107351f7635cad3108abd63f514
julia <771450210@qq.com>
2018-04-27 17:06:59 +0800

    20180426 commit

---
+++ Common/EmailHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    /// <summary>
+    /// 概述：邮箱公共类
+    /// 文件：Common.EmailHelper
+    /// 作者：詹詹    创建时间：2018/4/27 16:34:02
+    /// 描述：
+    ///    > add description for EmailHelper
+    ///    修改历史：
+    ///
+    /// <summary>
+    public class EmailHelper
+    {
+
+    }
+}
