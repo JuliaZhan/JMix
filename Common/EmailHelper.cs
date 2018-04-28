ba77460019cb701f436b49494e0fd74c99a9971b
julia <771450210@qq.com>
2018-04-28 13:18:43 +0800

    20180428 commit

--- Common/EmailHelper.cs
+++ Common/EmailHelper.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 
 namespace Common
 {
@@ -17,6 +16,171 @@ namespace Common
     /// <summary>
     public class EmailHelper
     {
+        private static ExchangeService _exchangeService = new ExchangeService(ExchangeVersion.Exchange2013);
+        /// <summary>
+        /// 获取收件箱
+        /// </summary>
+        /// <param name="userId">当前用户名</param>
+        /// <param name="pwd">密码</param>
+        /// <param name="domain">域</param>
+        /// <param name="pageSize">一次加载的数量</param>
+        /// <param name="offset">偏移量</param>
+        /// <returns></returns>
+        public static List<Email> GetInbox(string userId, string pwd, string domain, int pageSize, int offset)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(pwd) || string.IsNullOrEmpty(domain))
+                {
+                    throw new ArgumentNullException("当前用户信息为空，无法访问exchange服务器");
+                }
+                List<Email> lstEmails = new List<Email>();
+                _exchangeService.Credentials = new NetworkCredential(userId, pwd, domain);//发送邮箱
+                _exchangeService.Url = new Uri("https://mail.jcgroup.com.cn/Exchange.asmx");//服务器邮箱
+                ItemView view = new ItemView(pageSize, offset);
+                FindItemsResults<Item> findResults = _exchangeService.FindItems(WellKnownFolderName.Inbox, SetFilter(), view);
+                foreach (Item item in findResults.Items)
+                {
+                    item.Load(PropertySet.FirstClassProperties);
+                    lstEmails.Add(new Email()
+                    {
+                        ExchangeItemId = item.Id.ChangeKey,
+                        body = item.Body.Text,
+                        Mail_cc = item.DisplayCc,
+                        Mail_from = item.LastModifiedName,
+                        IsRead = item.IsNew,
+                        Subject = item.Subject,
+                        CreateOn = item.DateTimeCreated
+                    });
+                }
+                return lstEmails;
+            }
+            catch (Exception ex)
+            {
 
+                throw ex;
+            }
+
+        }
+        /// <summary>
+        /// 根据用户邮件地址返回用户的未读邮件数
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public static int GetUnReadMailCountByUserMailAddress(string userId, string pwd, string domain, string email)
+        {
+            int unRead = 0;
+            try
+            {
+                _exchangeService.Credentials = new NetworkCredential(userId, pwd, domain);
+                _exchangeService.Url = new Uri("https://mail.jcgroup.com.cn/Exchange.asmx");//服务器邮箱
+                _exchangeService.ImpersonatedUserId = new ImpersonatedUserId(ConnectingIdType.SmtpAddress, email);
+
+                unRead = Folder.Bind(_exchangeService, WellKnownFolderName.Inbox).UnreadCount;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return unRead;
+        }
+        /// <summary>
+        /// 过滤器
+        /// </summary>
+        /// <returns></returns>
+        private static SearchFilter SetFilter()
+        {
+            List<SearchFilter> searchFilterCollection = new List<SearchFilter>();
+            //searchFilterCollection.Add(new SearchFilter.IsEqualTo(EmailMessageSchema.IsRead, false));
+            //searchFilterCollection.Add(new SearchFilter.IsEqualTo(EmailMessageSchema.IsRead, true));
+            //筛选今天的邮件
+            SearchFilter start = new SearchFilter.IsGreaterThanOrEqualTo(EmailMessageSchema.DateTimeCreated, Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd 00:00:00")));
+            SearchFilter end = new SearchFilter.IsLessThanOrEqualTo(EmailMessageSchema.DateTimeCreated, Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd 23:59:59")));
+            searchFilterCollection.Add(start);
+            searchFilterCollection.Add(end);
+            SearchFilter filter = new SearchFilter.SearchFilterCollection(LogicalOperator.And, searchFilterCollection.ToArray());
+            return filter;
+        }
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public static void SendMail(Email email, string userId, string pwd, string domain)
+        {
+            try
+            {
+                _exchangeService.Credentials = new NetworkCredential(userId, pwd, domain);
+                _exchangeService.Url = new Uri("https://mail.jcgroup.com.cn/Exchange.asmx");//服务器邮箱
+                //发送人
+                Mailbox mail = new Mailbox(email.Mail_from);
+                //邮件内容
+                EmailMessage message = new EmailMessage(_exchangeService);
+                string[] strTos = email.Mail_to.Split(';');
+                //接收人
+                foreach (string item in strTos)
+                {
+                    if (!string.IsNullOrEmpty(item))
+                    {
+                        message.ToRecipients.Add(item);
+                    }
+                }
+                //抄送人
+                foreach (string item in email.Mail_cc.Split(';'))
+                {
+                    if (!string.IsNullOrEmpty(item))
+                    {
+                        message.CcRecipients.Add(item);
+                    }
+
+                }
+                //邮件标题
+                message.Subject = email.Subject;
+                //邮件内容
+                message.Body = new MessageBody(email.body);
+                //发送并且保存
+                message.SendAndSaveCopy();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("发送邮件出错，" + ex.Message + "\r\n" + ex.StackTrace);
+            }
+        }
+    }
+    public class Email
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ExchangeItemId { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string body { get; set; }
+        /// <summary>
+        /// 抄送
+        /// </summary>
+        public string Mail_cc { get; set; }
+        /// <summary>
+        /// 发件人
+        /// </summary>
+        public string Mail_from { get; set; }
+        /// <summary>
+        /// 收件人
+        /// </summary>
+        public string Mail_to { get; set; }
+        /// <summary>
+        /// 是否已读
+        /// </summary>
+        public bool IsRead { get; set; }
+        /// <summary>
+        /// 主题
+        /// </summary>
+        public string Subject { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateOn { get; set; }                       
     }
 }
---
+++ Common/WebRequestsHelper.cs
@@ -0,0 +1,171 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    /// <summary>
+    /// 概述：webRequests
+    /// 文件：Common.WebRequestsHelper
+    /// 作者：詹詹    创建时间：2018/4/28 11:28:56
+    /// 描述：
+    ///    > add description for WebRequestsHelper
+    ///    修改历史：
+    ///
+    /// <summary>
+    public class WebRequestsHelper
+    {
+        /// <summary>
+        /// 表单post请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="param">请求参数</param>
+        /// <param name="authorization">头部认证信息</param>
+        public static string sendPost(string url, Dictionary<string, string> param, string authorization)
+        {
+            //组合参数
+            string parameters = "";
+            if (param != null)
+            {
+                foreach (KeyValuePair<string, string> kv in param)
+                {
+                    parameters += parameters == "" ? "" : "&";
+                    parameters += kv.Key + "=" + kv.Value;
+                }
+            }
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            //设置头部认证信息
+            if (!string.IsNullOrEmpty(authorization))
+            {
+                request.Headers["Authorization"] = authorization;
+            }
+            request.ContentType = "application/x-www-form-urlencoded";
+            //传入post参数
+            request.ContentLength = parameters.Length;
+            StreamWriter writer = new StreamWriter(request.GetRequestStream(), Encoding.ASCII);
+            writer.Write(parameters);
+            writer.Flush();
+            //请求接口并获得返回值
+            HttpWebResponse response;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                //返回错误信息
+                response = (HttpWebResponse)ex.Response;
+            }
+            string encoding = response.ContentEncoding;
+            if (encoding == null || encoding.Length < 1)
+            {
+                encoding = "UTF-8";
+            }
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
+            return reader.ReadToEnd();
+        }
+        /// <summary>
+        /// 表达get请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="param">请求参数</param>
+        /// <param name="authorization">头部认证信息</param>
+        public static string sendGet(string url, Dictionary<string, string> param, string authorization)
+        {
+            //组合参数
+            string parameters = "";
+            if (param != null)
+            {
+                foreach (KeyValuePair<string, string> kv in param)
+                {
+                    parameters += parameters == "" ? "" : "&";
+                    parameters += kv.Key + "=" + kv.Value;
+                }
+            }
+            //拼接get参数
+            url += parameters == "" ? "" : "?" + parameters;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            //设置头部认证信息
+            request.Headers["Authorization"] = authorization;
+            request.ContentType = "application/x-www-form-urlencoded";
+            //请求接口并获得返回值
+            HttpWebResponse response;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                //返回错误信息
+                response = (HttpWebResponse)ex.Response;
+            }
+            string encoding = response.ContentEncoding;
+            if (encoding == null || encoding.Length < 1)
+            {
+                encoding = "UTF-8";
+            }
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
+            return reader.ReadToEnd();
+        }
+        /// <summary>
+        /// 将对象序列化为JSON格式
+        /// </summary>
+        /// <param name="o">对象</param>
+        /// <returns>json字符串</returns>
+        public static string SerializeObject(object o)
+        {
+            string json = JsonConvert.SerializeObject(o);
+            return json;
+        }
+
+        /// <summary>
+        /// 解析JSON字符串生成对象实体
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="json">json字符串(eg.{"ID":"112","Name":"石子儿"})</param>
+        /// <returns>对象实体</returns>
+        public static T DeserializeJsonToObject<T>(string json) where T : class
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            StringReader sr = new StringReader(json);
+            object o = serializer.Deserialize(new JsonTextReader(sr), typeof(T));
+            T t = o as T;
+            return t;
+        }
+
+        /// <summary>
+        /// 解析JSON数组生成对象实体集合
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="json">json数组字符串(eg.[{"ID":"112","Name":"石子儿"}])</param>
+        /// <returns>对象实体集合</returns>
+        public static List<T> DeserializeJsonToList<T>(string json) where T : class
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            StringReader sr = new StringReader(json);
+            object o = serializer.Deserialize(new JsonTextReader(sr), typeof(List<T>));
+            List<T> list = o as List<T>;
+            return list;
+        }
+
+        /// <summary>
+        /// 反序列化JSON到给定的匿名对象.
+        /// </summary>
+        /// <typeparam name="T">匿名对象类型</typeparam>
+        /// <param name="json">json字符串</param>
+        /// <param name="anonymousTypeObject">匿名对象</param>
+        /// <returns>匿名对象</returns>
+        public static T DeserializeAnonymousType<T>(string json, T anonymousTypeObject)
+        {
+            T t = JsonConvert.DeserializeAnonymousType(json, anonymousTypeObject);
+            return t;
+        }
+    }
+}
