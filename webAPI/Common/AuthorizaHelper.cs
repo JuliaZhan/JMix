8dd379697d4c85e21823c7a95d7bddf9af02485b
juliazhan <771450210@qq.com>
2018-05-08 17:48:18 +0800

    20180508

---
+++ webAPI/Common/AuthorizaHelper.cs
@@ -0,0 +1,46 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Security;
+
+namespace webAPI.Common
+{
+    public static class AuthorizaHelper
+    {
+        private static string msg = string.Empty;
+        public static string InitializeTicket(string custNo,string password)
+        {
+            FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(0, custNo, DateTime.Now, DateTime.Now.AddHours(1), false, string.Format("{0}&{1}", custNo, password));
+            var ticketString = FormsAuthentication.Encrypt(ticket);
+            HttpCookie cookie = new HttpCookie(ticketString, ticketString);
+            cookie.HttpOnly = true;
+            if(ticket.IsPersistent)
+            {
+                cookie.Expires = ticket.Expiration;
+            }
+            HttpContext context = HttpContext.Current;
+            if (context == null)
+                throw new InvalidOperationException();
+            context.Response.Cookies.Remove(cookie.Name);
+            context.Response.Cookies.Add(cookie);
+            var identity = new GenericIdentity(custNo);
+            identity.AddClaim(new System.Security.Claims.Claim(GenericIdentity.DefaultNameClaimType, custNo));
+            SetPrincipal(new GenericPrincipal(identity, null));
+            msg = string.Format("[User Ticket]:CNO:{0};T:{1};", custNo, ticketString);
+            Log.i(msg);
+            return ticketString;
+        }
+
+        private static void SetPrincipal(GenericPrincipal principal)
+        {
+            //throw new NotImplementedException();
+            Thread.CurrentPrincipal = principal;
+            if (HttpContext.Current != null)
+                HttpContext.Current.User = principal;
+        }
+    }
+}
---
+++ webAPI/Common/JAuthorizeAttribute.cs
@@ -0,0 +1,74 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Security;
+
+namespace webAPI.Common
+{
+    /// <summary>
+    /// 获取
+    /// </summary>
+    public class JAuthorizeAttribute:AuthorizeAttribute
+    {
+        private string msg = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="actionContext"></param>
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            //base.OnAuthorization(actionContext);
+            var authorization = actionContext.Request.Headers.Authorization;
+            msg = string.Format("[JAuthorization]Action:{0},Authorization:{1}", actionContext.Request.RequestUri.AbsolutePath, authorization);
+            Log.i(msg);
+            if(authorization!=null && authorization.Parameter!=null)
+            {
+                var encryptTicket = authorization.Parameter;
+                if(ValidateTicket(encryptTicket))
+                {
+                    base.IsAuthorized(actionContext);
+                }
+                else
+                {
+                    HandleUnauthorizedRequest(actionContext);
+                }
+            }
+            else
+            {
+                var attributes = actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().OfType<AllowAnonymousAttribute>();
+                bool isAnonymous = attributes.Any(a => a is AllowAnonymousAttribute);
+                if (isAnonymous) base.OnAuthorization(actionContext);
+                else HandleUnauthorizedRequest(actionContext);
+            }
+        }
+
+        private bool ValidateTicket(string encryptTicket)
+        {
+            //throw new NotImplementedException();
+            try
+            {
+                var ticket = FormsAuthentication.Decrypt(encryptTicket);
+                if(!ticket.IsPersistent && ticket.Expiration<DateTime.Now)
+                {
+                    Log.i("身份验证过期");
+                    return false;
+                }
+                var strTicket = ticket.UserData;
+                msg = string.Format("[Ticket String]:{0};", strTicket);
+                var index = strTicket.IndexOf("&");
+                string custno = strTicket.Substring(0, index);
+                string custpwd = strTicket.Substring(index + 1);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Log.e("ticket错误",ex);                
+            }
+            return false;
+        }
+    }
+}
