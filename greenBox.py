fb589d3ba27d976090851f55a7d186a63ee2f654
juliazhan <771450210@qq.com>
2018-05-20 00:00:00 +0800

    test lala

--- greenBox.py
+++ greenBox.py
@@ -1,17 +1,19 @@
 import os
 import datetime
 
-commit_date =datetime.datetime.strptime('2018-05-22 9:11:21','%Y-%m-%d %H:%M:%S') 
-now=datetime.datetime.strptime('2018-05-26 9:11:21','%Y-%m-%d %H:%M:%S')
+start='2018-05-20'
+end='2018-05-27'
+tiemstart=datetime.datetime.strptime(start,'%Y-%m-%d')
+timeend=datetime.datetime.strptime(end,'%Y-%m-%d')
 
-while commit_date < now:
+while tiemstart < timeend:
     os.system('git init ')
     os.system('git add .')
-    os.system('git commit --date={time} -m "test lala" '.format(time=commit_date.isoformat()))
+    os.system('git commit --date={time} -m "test lala" '.format(time=tiemstart.isoformat()))
     os.system('git push -u HZJC0001L master')
-    print(commit_date)
-    commit_date=datetime.timedelta(days=1)
-    print(commit_date)
+    print(tiemstart)
+    tiemstart+=datetime.timedelta(days=1)
+    print(tiemstart)
 
 
 
