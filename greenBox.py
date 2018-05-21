1e3a1180287af5369d16ba3499f03cef94a0b828
juliazhan <771450210@qq.com>
2018-05-22 00:00:00 +0800

    test lala

--- greenBox.py
+++ greenBox.py
@@ -1,16 +1,18 @@
 import os
 import datetime
 
-start='2018-05-20'
-end='2018-05-27'
+start='2018-05-22'
+end='2018-05-26'
 tiemstart=datetime.datetime.strptime(start,'%Y-%m-%d')
 timeend=datetime.datetime.strptime(end,'%Y-%m-%d')
 
 while tiemstart < timeend:
+    f=open('data.txt','a+')
     os.system('git init ')
     os.system('git add .')
     os.system('git commit --date={time} -m "test lala" '.format(time=tiemstart.isoformat()))
     os.system('git push -u HZJC0001L master')
+    f.writelines(tiemstart.isoformat() + '\n')
     print(tiemstart)
     tiemstart+=datetime.timedelta(days=1)
     print(tiemstart)
