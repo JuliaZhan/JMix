973befb1b7ff0495404256e081a2d7de41155d8a
juliazhan <771450210@qq.com>
2018-05-22 09:11:21 +0800

    test lala

--- greenBox.py
+++ greenBox.py
@@ -1,6 +1,17 @@
 import os
+import datetime
 
-os.system('git init ')
-os.system('git add .')
-os.system('git commit --date=2018-5-26 -m "test lala" ')
-os.system('git push -u HZJC0001L master')
+commit_date =datetime.datetime.strptime('2018-05-22 9:11:21','%Y-%m-%d %H:%M:%S') 
+now=datetime.datetime.strptime('2018-05-26 9:11:21','%Y-%m-%d %H:%M:%S')
+
+while commit_date < now:
+    os.system('git init ')
+    os.system('git add .')
+    os.system('git commit --date={time} -m "test lala" '.format(time=commit_date.isoformat()))
+    os.system('git push -u HZJC0001L master')
+    print(commit_date)
+    commit_date=datetime.timedelta(days=1)
+    print(commit_date)
+
+
+
